9069eb37fffb640dd9a12c3a34bb8d6dc1f4fdbd
Scott Weimer <sweimer65@hotmail.com>
2025-03-26 17:04:19 -0400

    add apps to be

---
+++ backend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ActivityForm from '../../frontend/src/components/ActivityForm';
+import '../../frontend/src/styles/main.scss';
+
+function App() {
+    return (
+        <div>
+            <ActivityForm/>
+        </div>
+    );
+}
+
+export default App;
