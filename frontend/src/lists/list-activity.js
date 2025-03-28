4e5b109bbc7ef35f19a165099fca614f4f959054
Scott Weimer <sweimer65@hotmail.com>
2025-03-28 14:42:31 -0400

    updated activites list

--- frontend/src/lists/list-activity.js
+++ frontend/src/lists/list-activity.js
@@ -13,12 +13,14 @@ const activityListItems = [
     {
         category: "MORNING",
         items: [
-            "Alex picked the pink dress with black tights from the outfits hanging outside her dresser.",
             "Sponsor/Relief helped Alex get dressed and brush her teeth and hair.",
             "Sponsor/Relief helped Alex brush teeth and hair.",
+            "",
+            "Alex picked the pink dress with black tights from the outfits hanging outside her dresser.",
             "Sponsor/Relief helped Alex pick out an outfit from the ones hanging outside her dresser.",
             "Alex put her shoes on the wrong feet and Sponsor/Relief helped Alex put them on right.",
             "Alex put her dress on backwards and Sponsor/Relief helped Alex put it on right.",
+            "",
             "Alex cleaned her room and organized her shelves. Sponsor/Relief checked on Alex until she completed her chores."
         ]
     },
@@ -26,17 +28,26 @@ const activityListItems = [
         category: "BREAKFAST",
         items: [
             "Sponsor/Relief asked Alex if she wanted cereal or oats for breakfast. Alex picked Oats.",
+            "",
             "Alex said she wanted cereal for breakfast.",
             "Sponsor/Relief prepared cereal, monitored Alex as she ate and administered AM meds.",
+            "",
             "Sponsor/Relief already prepared breakfast.",
             "Sponsor/Relief monitored Alex as she ate and administered AM meds.",
             "Alex ate Breakfast and was monitored by Sponsor/Relief.",
             "Sponsor/Relief administer AM meds.",
+            "",
+            "While eating. Sponsor, Relief and Alex discussed the menu for the upcoming week and made a small shopping list.",
+            "",
             "Alex helped clear the table.",
             "Sponsor/Relief made Alex tea and made sure it was the right temperature.",
             "Alex drank her tea until the van arrived.",
             "Alex watched TV until the van arrived.",
+            "",
             "Van arrived at 830am to take Alex to Diversity.",
+            "Van returned at 230pm.",
+            "",
+            "Kiearra returned at 330pm.",
             "Kiearra arrived at 800am to take Alex out for a girl's day."
         ]
     },
@@ -44,13 +55,27 @@ const activityListItems = [
         category: "MIDDAY",
         items: [
             "Sponsor/Relief and Alex took Izzy for a quick neighborhood walk.",
-            "Sponsor/Relief asked Alex if she wanted to work on her new pop-together beads. Alex said Yes and worked on them in the Family room for a while.",
-            "Sponsor/Relief ran a fire drill, helped Alex respond to the alarm and helped Alex evacuate to the meeting place in the front yard.",
-            "Sponsor/Relief and Alex took Izzy for a quick neighborhood walk.",
             "Sponsor/Relief and Alex took Izzy to Huguenot Park. Alex walked 1.85 miles.",
             "Sponsor/Relief and Alex took Izzy to the water tower to walk a couple laps around the track.",
+            "Sponsor/Relief and Alex took Izzy to the senior center to chase balls. Alex walked several laps around the field.",
             "Sponsor/Relief and Alex took Izzy back home.",
-            "Sponsor/Relief and Alex went to Wegmans and picked up the groceries on the list. Alex helped push the cart, load the car and unload the car at home."
+            "",
+            "Relief and Alex worked on making a bead bracelets while watching TV.",
+            "Sponsor/Relief asked Alex if she wanted to work on a beading craft. Alex said yes. Sponsor set Alex up with beads in the family room and checked in on her as she worked on it.",
+            "",
+            "Alex cleaned her room and organized her shelves. Sponsor/Relief checked on Alex until she completed her chores.",
+            "Alex helped Sponsor put away some laundry.",
+            "Sponsor and Alex picked out outfits for the upcoming week at Diversity and hung them outside her dresser.",
+            "",
+            "Sponsor/Relief went to Target.",
+            "Sponsor/Relief and Alex went to Wegmans and picked up the groceries on the list. Alex helped push the cart, load the car and unload the car at home.",
+            "Relief and Alex waited in the car while Sponsor shopped.",
+            "Alex helped push the car, load the car and unload the car back home.",
+            "Sponsor/Relief and Alex took Izzy back home.",
+            "",
+            "Sponsor, Relief and Alex watched a movie on TV.",
+            "",
+            "Sponsor/Relief ran a fire drill, helped Alex respond to the alarm and helped Alex evacuate to the meeting place in the front yard."
         ]
     },
     {
@@ -60,6 +85,33 @@ const activityListItems = [
             "Alex watched a little tv while Sponsor/Relief prepared lunch.",
             "Alex helped Sponsor cook home-made granola.",
             "Alex helped clear the table."
+        ]
+    },
+    {
+        category: "AFTERNOON/AFTER LUNCH",
+        items: [
+            "Sponsor/Relief and Alex took Izzy for a quick neighborhood walk.",
+            "Sponsor/Relief and Alex took Izzy to Huguenot Park. Alex walked 1.85 miles.",
+            "Sponsor/Relief and Alex took Izzy to the water tower to walk a couple laps around the track.",
+            "Sponsor/Relief and Alex took Izzy to the senior center to chase balls. Alex walked several laps around the field.",
+            "Sponsor/Relief and Alex took Izzy back home.",
+            "",
+            "Relief and Alex worked on making a bead bracelets while watching TV.",
+            "Sponsor/Relief asked Alex if she wanted to work on a beading craft. Alex said yes. Sponsor set Alex up with beads in the family room and checked in on her as she worked on it.",
+            "",
+            "Alex cleaned her room and organized her shelves. Sponsor/Relief checked on Alex until she completed her chores.",
+            "Alex helped Sponsor put away some laundry.",
+            "Sponsor and Alex picked out outfits for the upcoming week at Diversity and hung them outside her dresser.",
+            "",
+            "Sponsor/Relief went to Target.",
+            "Sponsor/Relief and Alex went to Wegmans and picked up the groceries on the list. Alex helped push the cart, load the car and unload the car at home.",
+            "Relief and Alex waited in the car while Sponsor shopped.",
+            "Alex helped push the car, load the car and unload the car back home.",
+            "Sponsor/Relief and Alex took Izzy back home.",
+            "",
+            "Sponsor, Relief and Alex watched a movie on TV.",
+            "",
+            "Sponsor/Relief ran a fire drill, helped Alex respond to the alarm and helped Alex evacuate to the meeting place in the front yard."
         ]
     },
     {
@@ -73,46 +125,30 @@ const activityListItems = [
         ]
     },
     {
-        category: "CHORE",
+        category: "EVENING",
         items: [
+            "Sponsor/Relief and Alex took Izzy for a quick neighborhood walk.",
+            "Sponsor/Relief and Alex took Izzy to Huguenot Park. Alex walked 1.85 miles.",
+            "Sponsor/Relief and Alex took Izzy to the water tower to walk a couple laps around the track.",
+            "Sponsor/Relief and Alex took Izzy to the senior center to chase balls. Alex walked several laps around the field.",
+            "Sponsor/Relief and Alex took Izzy back home.",
+            "",
+            "Relief and Alex worked on making a bead bracelets while watching TV.",
+            "Sponsor/Relief asked Alex if she wanted to work on a beading craft. Alex said yes. Sponsor set Alex up with beads in the family room and checked in on her as she worked on it.",
+            "",
             "Alex cleaned her room and organized her shelves. Sponsor/Relief checked on Alex until she completed her chores.",
             "Alex helped Sponsor put away some laundry.",
-            "Sponsor and Alex picked out outfits for the upcoming week at Diversity and hung them outside her dresser."
-        ]
-    },
-    {
-        category: "MENU PLANNING and GROCERY SHOPPING",
-        items: [
-            "While eating. Sponsor, Relief and Alex discussed the menu for the upcoming week and made a small shopping list.",
-        ]
-    },
-    {
-        category: "OUTING",
-        items: [
+            "Sponsor and Alex picked out outfits for the upcoming week at Diversity and hung them outside her dresser.",
+            "",
             "Sponsor/Relief went to Target.",
             "Sponsor/Relief and Alex went to Wegmans and picked up the groceries on the list. Alex helped push the cart, load the car and unload the car at home.",
             "Relief and Alex waited in the car while Sponsor shopped.",
             "Alex helped push the car, load the car and unload the car back home.",
-            "Sponsor/Relief asked Alex if she wanted to work on a beading craft. Alex said yes. Sponsor set Alex up with beads in the family room and checked in on her as she worked on it.",
-            "Relief and Alex worked on making pop-together bracelets while watching TV."
-        ]
-    },
-    {
-        category: "AFTERNOON/AFTER SCHOOL/EVENING",
-        items: [
-            "Van returned at 230pm.",
-            "Kiearra returned at 330pm.",
-            "Alex helped Sponsor put away some laundry.",
-            "Sponsor and Alex picked out outfits for the upcoming week at Diversity and hung them outside her dresser.",
-            "Sponsor/Relief and Alex took Izzy for a quick neighborhood walk.",
-            "Sponsor/Relief asked Alex if she wanted to work on her new pop-together beads. Alex said Yes and worked on them in the Family room for a while.",
-            "Sponsor/Relief ran a fire drill, helped Alex respond to the alarm and helped Alex evacuate to the meeting place in the front yard.",
-            "Sponsor/Relief and Alex took Izzy for a quick neighborhood walk.",
-            "Sponsor/Relief and Alex took Izzy to Huguenot Park. Alex walked 1.85 miles.",
-            "Sponsor/Relief and Alex took Izzy to the water tower to walk a couple laps around the track.",
-            "Sponsor/Relief and Alex took Izzy to the senior center to chase balls. alex walked several laps around the field.",
             "Sponsor/Relief and Alex took Izzy back home.",
-            "Sponsor, Relief and Alex watched a movie on TV."
+            "",
+            "Sponsor, Relief and Alex watched a movie on TV.",
+            "",
+            "Sponsor/Relief ran a fire drill, helped Alex respond to the alarm and helped Alex evacuate to the meeting place in the front yard."
         ]
     },
     {
