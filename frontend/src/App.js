2acab2a9643da715637e9b066b276bfbc133cb45
Scott Weimer <sweimer65@hotmail.com>
2026-03-12 16:43:41 -0400

    make app resize

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ActivityForm from './components/ActivityForm';
 import './styles/main.scss';
 
 function App() {
+
+    useEffect(() => {
+        const sendHeight = () => {
+            const height = document.body.scrollHeight;
+            window.parent.postMessage({ hudxAppHeight: height }, "*");
+        };
+
+        // Send initial height
+        sendHeight();
+
+        // Send height on resize
+        window.addEventListener("resize", sendHeight);
+
+        // Send height when content changes
+        const observer = new MutationObserver(sendHeight);
+        observer.observe(document.body, { childList: true, subtree: true });
+
+        return () => {
+            window.removeEventListener("resize", sendHeight);
+            observer.disconnect();
+        };
+    }, []);
+
     return (
         <div>
             <ActivityForm/>
